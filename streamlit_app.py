ced7d38a16c1af616bd24629fa86fe3e8209d75e
Piyush0823 <162396912+Piyush0823@users.noreply.github.com>
2025-08-06 16:54:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,13 @@ st.markdown(footer, unsafe_allow_html=True)
 
 st.markdown("""
 <style>
-
+.comparison-box {
+    background-color: #f8f9fa;
+    color: #F54927;
+    padding: 1.5rem;
+    border-radius: 10px;
+    border-left: 4px solid #667eea;
+}
 .st-emotion-cache-155jwzh{
     background-color: #450F1D;
     }
@@ -469,6 +475,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
